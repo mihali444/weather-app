c5f13f53b2b61c844ad202d2547d2512aa94cf54
Михаил <142327464+mihali444@users.noreply.github.com>
2023-08-16 13:40:59 +0300

    Create function

--- js/main.js
+++ js/main.js
@@ -5,53 +5,68 @@ const header = document.querySelector('.header')
 const form = document.querySelector('#form')
 const input = document.querySelector('#input-city')
 
+function removeCard() {
+    const prevCard = document.querySelector('.card')
+     if (prevCard) prevCard.remove()
+}
+
+function showCard(name, country, temp, condition) {
+
+    const html = `<div class="card">
+                    <h2 class="card-city">${name} <span>${country}</span></h2>
+
+                    <div class="card-wather">
+                        <div class="card-value">${temp}<sup>°c</sup></div>
+                        <img class="card-img" src="./img/weather1.png" alt="Weather">
+                    </div>
+
+                    <div class="card-desc">${condition}</div>
+
+                </div>`
+
+        header.insertAdjacentHTML('afterend', html)
+}
+
+function showError(errorMessage) {
+    const html = `<div class="card">${errorMessage}</div>`
+
+    header.insertAdjacentHTML('afterend', html)
+}
+
+async function getWeather (city) {
+    // Делаем запрос на сервер
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
 // Слушаем отправку формы
-form.onsubmit = function (e) {
+form.onsubmit = async function (e) {
 
     // Отменяем отправку формы
     e.preventDefault()
 
     // Берём значение из инпута, обрезаем пробелы
     let city = input.value.trim()
-    
-    // Делаем запрос на сервер
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
 
-    fetch(url).then((response) => {
-        return response.json()
-    })
-    .then((data) => {
+    const data = await getWeather(city)
+
+    if (data.error) {
+        removeCard()
+        showError(data.error.message)
+
+    } else {
         
-        //Проверка на ошибки
+        removeCard()
+        showCard(
+            data.location.name,
+            data.location.country, 
+            data.current.temp_c, 
+            data.current.condition.text
+        )
+    
 
-        if (data.error) {
-            const prevCard = document.querySelector('.card')
-            if (prevCard) prevCard.remove()
-
-            const html = `<div class="card">${data.error.message}</div>`
-
-            header.insertAdjacentHTML('afterend', html)
-        } else {
-            //Удаляем предыдущую карточку
-        const prevCard = document.querySelector('.card')
-        if (prevCard) prevCard.remove()
-
-        // Разметка для карточки
-        const html = `<div class="card">
-                    <h2 class="card-city">${data.location.name} <span>${data.location.country}</span></h2>
-
-                    <div class="card-wather">
-                        <div class="card-value">${data.current.temp_c}<sup>°c</sup></div>
-                        <img class="card-img" src="./img/weather1.png" alt="Weather">
-                    </div>
-
-                    <div class="card-desc">${data.current.condition.text}</div>
-
-                </div>`
-
-        //Отображаем карточку на странице
-        header.insertAdjacentHTML('afterend', html)
-        }
-    })
+    }
 
 }
